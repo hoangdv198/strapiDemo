494228ca149d1e6992a2a8ecd5c19ef1ffe09cdb
Dang Hoang <hoangdv@icts.vn>
2022-04-29 17:45:14 +0700

    clear base

--- src/api/restaurantv/routes/restaurantv.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * restaurantv router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::restaurantv.restaurantv');
--- src/api/restaurantv/services/restaurantv.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * restaurantv service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::restaurantv.restaurantv');
